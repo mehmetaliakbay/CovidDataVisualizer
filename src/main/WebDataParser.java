48a5761537d643d4405d6ccfa8d814197548f848
mehmet ali akbay <mhmtakbay@gmail.com>
2020-05-30 18:42:00 +0300

    for save xml text in StringBuilder changed Scanner to BufferedReader

--- src/main/WebDataParser.java
+++ src/main/WebDataParser.java
@@ -2,7 +2,9 @@ package main;
 
 import main.model.CoronaData;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.*;
 import java.util.regex.Matcher;
@@ -21,17 +23,29 @@ public class WebDataParser {
 
     public static List<CoronaData> parseEntry(URL url) throws Exception {
         list = new ArrayList<CoronaData>();
-        InputStream in = url.openStream();
-        Scanner scan = new Scanner(in);
-        StringBuilder sb = new StringBuilder();
-
-
-        while (scan.hasNext()) {
-            String str = scan.nextLine();
-            sb.append(str);
-
+        InputStream in = null;
+        BufferedReader br;
+        StringBuilder sb=null;
+        String line;
+        try {
+            in = url.openStream();
+            br = new BufferedReader(new InputStreamReader(in));
+            sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        scan.close();
+
 
         //split <record> </record> pattern
         Pattern groupPattern = Pattern.compile("(<record>)(.*?)(</record>)");
@@ -47,45 +61,45 @@ public class WebDataParser {
             stringBuilder.append(s);
 
             // for time parsing
-            String time = parseRecord(stringBuilder,"(<dateRep>)(.*?)(</dateRep>)");
+            String time = parseRecord(stringBuilder, "(<dateRep>)(.*?)(</dateRep>)");
 
 
             // for day parsing
-            Integer day = Integer.parseInt(parseRecord(stringBuilder,"(<day>)(.*?)(</day>)"));
+            Integer day = Integer.parseInt(parseRecord(stringBuilder, "(<day>)(.*?)(</day>)"));
 
 
             // for month parsing
-            Integer month = Integer.parseInt(parseRecord(stringBuilder,"(<month>)(.*?)(</month>)"));
+            Integer month = Integer.parseInt(parseRecord(stringBuilder, "(<month>)(.*?)(</month>)"));
 
             // for year parsing
-            Integer year =  Integer.parseInt(parseRecord(stringBuilder,"(<year>)(.*?)(</year>)"));
+            Integer year = Integer.parseInt(parseRecord(stringBuilder, "(<year>)(.*?)(</year>)"));
 
             // for newcase parsing
-            Integer newCase =  Integer.parseInt(parseRecord(stringBuilder,"(<cases>)(.*?)(</cases>)"));
+            Integer newCase = Integer.parseInt(parseRecord(stringBuilder, "(<cases>)(.*?)(</cases>)"));
 
             // for newdeath parsing
-            Integer newDeath= Integer.parseInt(parseRecord(stringBuilder,"(<deaths>)(.*?)(</deaths>)"));
+            Integer newDeath = Integer.parseInt(parseRecord(stringBuilder, "(<deaths>)(.*?)(</deaths>)"));
 
             // for country parsing
-            String country = parseRecord(stringBuilder,"(<countriesAndTerritories>)(.*?)(</countriesAndTerritories>)");
+            String country = parseRecord(stringBuilder, "(<countriesAndTerritories>)(.*?)(</countriesAndTerritories>)");
 
             //for population parsing
             Integer population;
-            int populationLength = parseRecord(stringBuilder,"(<popData2018>)(.*?)(</popData2018>)").length();
-            if(populationLength<2){
+            int populationLength = parseRecord(stringBuilder, "(<popData2018>)(.*?)(</popData2018>)").length();
+            if (populationLength < 2) {
                 population = 0;
-            }else{
-                population = Integer.parseInt(parseRecord(stringBuilder,"(<popData2018>)(.*?)(</popData2018>)"));
+            } else {
+                population = Integer.parseInt(parseRecord(stringBuilder, "(<popData2018>)(.*?)(</popData2018>)"));
 
             }
 
             //for continent
-            String continent = parseRecord(stringBuilder,"(<continentExp>)(.*?)(</continentExp>)");
-            Integer totalDeath=0;
-            Integer totalCases=0;
-            if(list !=null){
+            String continent = parseRecord(stringBuilder, "(<continentExp>)(.*?)(</continentExp>)");
+            Integer totalDeath = 0;
+            Integer totalCases = 0;
+            if (list != null) {
                 for (CoronaData cd : list) {
-                    if (cd.getCountry().equals(country)){
+                    if (cd.getCountry().equals(country)) {
                         totalDeath += cd.getNewDeath();
                         totalCases += cd.getNewCase();
 
@@ -93,7 +107,7 @@ public class WebDataParser {
                 }
             }
 
-            list.add(new CoronaData(time,day,month,year,newCase,newDeath,country,population,continent,totalDeath,totalCases));
+            list.add(new CoronaData(time, day, month, year, newCase, newDeath, country, population, continent, totalDeath, totalCases));
 
         }
 
@@ -101,8 +115,7 @@ public class WebDataParser {
     }
 
 
-
-    private static String parseRecord(StringBuilder stringBuilder,String regEx) {
+    private static String parseRecord(StringBuilder stringBuilder, String regEx) {
         Pattern groupPattern = Pattern.compile(regEx);
         String record = null;
         Matcher groupMatcher = groupPattern.matcher(stringBuilder);
